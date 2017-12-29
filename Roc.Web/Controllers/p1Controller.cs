f65df1b398a04d6e9137bc30e335bc9d489ccb73
laozhao <3@3.com>
2017-12-30 00:21:48 +0800

    ok

--- Roc.Web/Controllers/p1Controller.cs
+++ Roc.Web/Controllers/p1Controller.cs
@@ -8,10 +8,12 @@ namespace Roc.Web.Controllers
 {
     public class p1Controller : Controller
     {
-        //
-        // GET: /p1/
-
+        //赵忠祥第一次做的页面
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Fuall()
         {
             return View();
         }
